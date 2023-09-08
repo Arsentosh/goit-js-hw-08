e524dbf12e793f1d4777dc0143c091e266fd0427
Arsentosh <doncenkoarsenij185@gmail.com>
2023-09-08 15:30:25 +0300

    fix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,18 +1,26 @@
-document.addEventListener('DOMContentLoaded', function () {
-  // Создаем Vimeo.Player после загрузки API
-  const iframe = document.querySelector('iframe');
-  const player = new Vimeo.Player(iframe);
+import Player from '@vimeo/player';
 
-  // Восстанавливаем время воспроизведения при загрузке страницы
-  const savedTime = localStorage.getItem('videoplayer-current-time');
-  if (savedTime) {
-    player.setCurrentTime(parseFloat(savedTime));
-  }
+// Создаем Vimeo.Player после загрузки API
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
-  player.on('play', function () {
-    // Сохраняем текущее время воспроизведения в локальное хранилище
-    player.getCurrentTime().then(function (timeupdate) {
+let currentTimeInLocalStorage = null;
+
+// Восстанавливаем время воспроизведения при загрузке страницы
+const savedTime = localStorage.getItem('videoplayer-current-time');
+if (savedTime !== null) {
+  const currentTime = parseFloat(savedTime);
+  currentTimeInLocalStorage = currentTime;
+  player.setCurrentTime(currentTime);
+} else {
+  console.log('Сохраненное время не найдено или не задано.');
+}
+
+player.on('timeupdate', function () {
+  // Сохраняем текущее время воспроизведения в локальное хранилище
+  player.getCurrentTime().then(function (timeupdate) {
+    if (currentTimeInLocalStorage === null) {
       localStorage.setItem('videoplayer-current-time', timeupdate);
-    });
+    }
   });
 });
