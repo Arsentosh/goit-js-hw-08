99287cf2ad406277485de1bdb0ae2d337ed8f3a5
Arsentosh <125693402+Arsentosh@users.noreply.github.com>
2023-09-08 12:30:51 +0000

    Deploying to gh-pages from @ Arsentosh/goit-js-hw-08@e524dbf12e793f1d4777dc0143c091e266fd0427 🚀

--- 02-video.c8891e30.js
+++
@@ -1,2 +0,0 @@
-document.addEventListener("DOMContentLoaded",(function(){var e=document.querySelector("iframe"),t=new Vimeo.Player(e),n=localStorage.getItem("videoplayer-current-time");n&&t.setCurrentTime(parseFloat(n)),t.on("play",(function(){t.getCurrentTime().then((function(e){localStorage.setItem("videoplayer-current-time",e)}))}))}));
-//# sourceMappingURL=02-video.c8891e30.js.map
